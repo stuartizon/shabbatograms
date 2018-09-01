bb9379cf49e9a5dee2e68ae62dc87e5c02a15dd8
Stuart Izon <stuart.izon@gmail.com>
2018-09-01 10:31:45 +1000

    Test an idea

--- src/Server.ts
+++ src/Server.ts
@@ -5,14 +5,14 @@ import { writePDF } from './PDF';
 
 export function Server(port: number) {
     const app = express();
-    app.use(urlencoded());
+    app.use(urlencoded({ extended: false }));
 
     app.post('/send', (req, res) => {
         const text = convertRequestToText(req.body);
         writePDF(text, res);
     });
 
-    app.use(express.static('public'));
+    // app.use(express.static('public'));
 
     app.listen(port, () => console.log(`Shabbatograms started on port ${port}!`));
 }
